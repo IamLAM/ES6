e6db8177e607dafa37d7b83fec918c39c6383302
IamLAM <ingeniero@luismonge.com.mx>
2019-04-03 13:11:09 -0700

    var and let

---
+++ code.js
@@ -0,0 +1,10 @@
+//using ECMAScript 6
+
+//When we are working with ES6, we have some differences in var and let
+var t = "time";
+var t = "hour";
+console.log(t); // "hour"
+
+//let t2 = "time";
+let t2 = "hour"; // we'll have an error, because we are using the same variable
+console.log(t2);
