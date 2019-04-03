86a24cd3e36ea02835f0c4f30c3fcf68bb84d4bc
IamLAM <ingeniero@luismonge.com.mx>
2019-04-03 13:22:35 -0700

    Using const

--- code.js
+++ code.js
@@ -8,3 +8,9 @@ console.log(t); // "hour"
 //let t2 = "time";
 let t2 = "hour"; // we'll have an error, because we are using the same variable
 console.log(t2);
+
+//const variable
+
+const PI = 3.141516;
+//PI = 22; // we'll have an error, because the variable is const
+console.log(PI);
